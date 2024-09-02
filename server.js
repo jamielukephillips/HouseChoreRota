7cee86debba1ea0dc2a125678f11feb605bfb65a
jpsolirius <jamie.phillips@solirius.com>
2024-09-02 09:36:35 +0100

    Add leaderboard functionality

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
+app.get('/leaderboard', (req, res) => {
+    console.log('Received request for /leaderboard');
+    res.sendFile(path.join(__dirname, 'public', 'leaderboard.html'));
+});
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
