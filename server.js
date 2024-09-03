6c7cfd668a19432f5363a3e1431fb6dff4d84c02
jpsolirius <jamie.phillips@solirius.com>
2024-09-03 11:45:17 +0100

    Date format changes

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const app = express();
 const PORT = 3000;
+
+const corsOptions = {
+    origin: 'https://h0useh0ld-ch0r3z.web.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+};
+
+app.use(cors(corsOptions));
 
 // Serve static files from the "assets" directory
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
